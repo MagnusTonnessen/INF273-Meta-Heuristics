e4ab8a324b50e0f1294f203425475f4437f13206
Magnus <magnus.tonnessen97@gmail.com>
2021-02-15 19:46:03 +0100

    The spaghetti is fresh

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        pdpUtils utils = new pdpUtils();
+        utils.loadProblem("src\\main\\resources\\Call_7_Vehicle_3.txt");
+    }
+}
